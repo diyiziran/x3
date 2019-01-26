6d5f1e33e554c11697cbf5c143211c3eeefee189
diyiziran <diyiziran@163.com>
2019-01-26 23:18:06 +0800

    from home

--- py/double_color_ball.py
+++ py/double_color_ball.py
@@ -11,7 +11,8 @@ import urllib3
 
 #利用urllib2获取网络数据
 http = urllib3.PoolManager()
-url ="http://f.apiplus.net/ssq.json"        
+#url ="http://f.apiplus.net/ssq.json"     
+url ="http://caipiaojieguo.com/api/lottrey?biaoshi=ssq&format=json&rows=10" 
 r = http.request('GET', url)
 value=json.loads(r.data)
 rootlist=value.keys()
@@ -19,9 +20,10 @@ for rootkey in rootlist:
     print(rootkey)
 datas=value['data']
 
-#逐个号码解析出来
 num=len(datas)
 i=0
+#逐个号码解析出来
+
 #while(i<num):
 #    str_code=datas[i]['opencode']
 #    blue_ball=str_code[-2:]
@@ -36,13 +38,21 @@ i=0
 #检索是否中奖
 targe_red_ball=['13', '03', '14', '16', '25', '28']
 while(i<num):
-    str_code=datas[i]['opencode']
+#    from彩票结果网
+    str_code=datas[i]['result']
     blue_ball=str_code[-2:]
     print('blue ball: ',blue_ball)
     red_ball_list=str_code[:-3].split(',')
+#    开彩网
+#    str_code=datas[i]['opencode']
+#    blue_ball=str_code[-2:]
+#    print('blue ball: ',blue_ball)
+#    red_ball_list=str_code[:-3].split(',')
+#    向字典加入内容项
     datas[i]['blue_ball']=blue_ball
     datas[i]['red_ball']=red_ball_list
-    print(datas[i]['expect']+':',red_ball_list)
+#    print(datas[i]['expect']+':',red_ball_list)
+    print(datas[i]['qishu']+':',red_ball_list)
     j=0
     result=[0,0,0,0,0,0]
     while(j<len(red_ball_list)):
@@ -51,5 +61,10 @@ while(i<num):
     print('中奖结果：',result)
     i+=1
     
-    
+#把字典写到文件    
+jsObj = json.dumps(datas)
+ 
+fileObject = open('double_color_ball.json', 'w')
+fileObject.write(jsObj)
+fileObject.close()
     
--- py/py_test.py
+++ py/py_test.py
@@ -1,20 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
 
 
-print ([x*x for x in range(1,10) if x%2==0])
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import json
+import urllib3
+
+#利用urllib2获取网络数据
+http = urllib3.PoolManager()
+url ="http://caipiaojieguo.com/api/lottrey?biaoshi=ssq&format=json&rows=10"        
+r = http.request('GET', url)
+value=json.loads(r.data)
+rootlist=value.keys()
+for rootkey in rootlist:
+    print(rootkey)
+datas=value['data']
 
 
-def count(m):
-    fs = []
-    for i in range(1, m):
-        def f():
-             return i*i
-        fs.append(f)
-    return fs
+dictObj = {
+	'andy':{
+		'age': 23,
+		'city': 'shanghai',
+		'skill': 'python'
+	},
+	'william': {
+		'age': 33,
+		'city': 'hangzhou',
+		'skill': 'js'
+	}
+}
+ 
+#写入json文件
+jsObj = json.dumps(dictObj)
+ 
+fileObject = open('jsonFile.json', 'w')
+fileObject.write(jsObj)
+fileObject.close()
 
-print (count(6))
+#读取json文件，生成一个dict
+file = open('test.txt', 'r') 
+js = file.read()
+dic = json.loads(js)   
+print(dic) 
+file.close()
 
 
-http://f.apiplus.net/ssq.json
+http://caipiaojieguo.com/api/lottrey?biaoshi=ssq&format=json&rows=10
