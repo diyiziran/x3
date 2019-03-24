f2dbec6e89d3d0ff144e890f9bc9c8ef7be02343
diyiziran <diyiziran@163.com>
2019-03-24 21:48:49 +0800

    *x*

--- py/quant.py
+++ py/quant.py
@@ -4,8 +4,8 @@
 
 import pandas as pd
 #import DataFrame as df
-
-
+import matplotlib.pyplot as plt
+import numpy as np
 import tushare as ts
 
 #2015年第三季度基金持股情况
@@ -19,3 +19,30 @@ df.s
 #设置通联数据的凭证
 ts.set_token('a6077ed0e7f1f4e3758f4beb973bb7180a5971026c6558a7ea57433a1533a064')
 ts.get_notices(code='000776.sh')
+
+
+#打印乘法口诀
+l1=list()
+for i in range(1,10):
+    l1.append(i)
+l2=random.sample(l1,9)
+l3=random.sample(l1,9)
+     
+for j in l3:
+    for i in l2:
+        print(str(j)+' x '+str(i)+' = ')
+    
+for j in range(7,10):
+    for i in l2:
+        print(str(j)+' x '+str(i)+' = ')    
+    
+f1=np.random.randint(9,size=9)
+
+for i in range(10):
+    for j in range(10):
+        print(str(i)+' x '+str(j)+' = ')
+        
+        
+for i in np.random.randint(1,10,size=9):
+    print('9 x '+str(i)+' = ')
+    
